7e2b3adcabecc309b433f708131c08ed957c1ed3
tiankx1003 <tiankx1003@gmail.com>
2019-10-18 12:19:07 +0800

    import implicit

--- FlinkTutorial/src/main/scala/com/tian/wc/BatchWordCount.scala
+++ FlinkTutorial/src/main/scala/com/tian/wc/BatchWordCount.scala
@@ -1,25 +1,30 @@
 package com.tian.wc
 
-import org.apache.flink.api.scala.{AggregateDataSet, DataSet, ExecutionEnvironment}
-
+import org.apache.flink.api.scala._
 /**
+ * 批处理wordcount
+ *
  * @author tian
  * @date 2019/10/18 10:00
  * @version 1.0.0
  */
-class WordCount {
+object BatchWordCount {
     def main(args: Array[String]): Unit = {
         // 创建执行环境
-        val env = ExecutionEnvironment.getExecutionEnvironment
+        val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
         // 从文件中读取数据
-        val inputPath = "files/hello.txt" // TODO: 更改路径
+        val inputPath: String = "files/hello.txt" // TODO: 更改路径
         val inputDS: DataSet[String] = env.readTextFile(inputPath)
         // 分词之后，对单词进行groupby分组，然后用sum进行聚合
         val wordCountDS: AggregateDataSet[(String, Int)] = inputDS
-            .flatMap(_.split(" ")).map((_, 1)).groupBy(0).sum(1)
+            .flatMap(_.split(" ")) //引入隐式转换
+            .map((_, 1))
+            .groupBy(0) //.groupBy(_._1)
+            .sum(1)
 
         // 打印输出
         wordCountDS.print()
+        //批处理中使用的为有界的流，不需要手动关闭资源
     }
 
 }
--- FlinkTutorial/src/main/scala/com/tian/wc/StreamWordCount.scala
+++ FlinkTutorial/src/main/scala/com/tian/wc/StreamWordCount.scala
@@ -1,9 +1,10 @@
 package com.tian.wc
 
-import org.apache.flink.api.java.utils.ParameterTool
-import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
+import org.apache.flink.streaming.api.scala._
 
 /**
+ * 流处理wordcount
+ *
  * @author tian
  * @date 2019/10/18 10:07
  * @version 1.0.0
@@ -11,19 +12,26 @@ import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironm
 object StreamWordCount {
     def main(args: Array[String]): Unit = {
         // 从外部命令中获取参数
-        val params: ParameterTool =  ParameterTool.fromArgs(args)
+        /*
+        val params: ParameterTool = ParameterTool.fromArgs(args)
         val host: String = params.get("host")
         val port: Int = params.getInt("port")
-
+        */
+        val host: String = "localhost"
+        val port: Int = 7777
         // 创建流处理环境
-        val env = StreamExecutionEnvironment.getExecutionEnvironment
+        val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
         // 接收socket文本流
         val textDstream: DataStream[String] = env.socketTextStream(host, port)
 
         // flatMap和Map需要引用的隐式转换
         import org.apache.flink.api.scala._
-        val dataStream: DataStream[(String, Int)] =
-            textDstream.flatMap(_.split("\\s")).filter(_.nonEmpty).map((_, 1)).keyBy(0).sum(1)
+        val dataStream: DataStream[(String, Int)] = textDstream
+            .flatMap(_.split("\\s"))
+            .filter(_.nonEmpty)
+            .map((_, 1))
+            .keyBy(0)
+            .sum(1)
 
         dataStream.print().setParallelism(1)
 
